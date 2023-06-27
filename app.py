e0f8633edd94642ee960461160b47ec356878b32
Ubuntu <azureuser@test.4foxr5ipgstu1gsig2blnvdzob.ex.internal.cloudapp.net>
2023-06-27 20:29:37 +0000

    Created html template files

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Evolution Simulator"
+    if "open" in request.form:
+        app.route("/open")
+    elif "close" in request.form:
+        return start()
+    return render_template("index.html")
 
 @app.route("/start")
 def start():
-    return "Start Simulation\nOpen Simulation"
+    return render_template("start.html")
+
+@app.route("/open")
+def open():
+    return render_template("open.html")
+
+@app.route("/simulation")
+def simulate():
+    return render_template("simulation.html")
+
+@app.route("/end")
+def end():
+    return render_template("end.html")
 
 if(__name__ == "__main__"):
     app.debug = True
