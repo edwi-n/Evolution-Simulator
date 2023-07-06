3fcd27e07a57d29870bd839ef1cfe0dff4d8e583
Ubuntu <azureuser@test.4foxr5ipgstu1gsig2blnvdzob.ex.internal.cloudapp.net>
2023-07-06 14:57:13 +0000

    Setup simulation.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request
+import sys
+# sys.path.insert(1, '~/Computer-Science-NEA/simulator')
+# import simulation.py
+from simulator import simulation
 
 app = Flask(__name__)
 
@@ -42,7 +46,20 @@ def open():
 @app.route("/simulation")
 def simulate():
     # return str(size)+"\n"+str(numberOfIterations)+"\n"+fitnessFunction
+    if str(numberOfIterations) == "" or str(size) == "" or fitnessFunction == "":
+        return app.redirect("/init-simulation-error")
     return render_template("simulation.html")
+
+@app.route("/init-simulation-error")
+def init_simulation_error():
+    error = ""
+    if str(numberOfIterations) == "" or numberOfIterations < 1 or numberOfIterations > 1000:
+        error += "Number of Iterations have to be > 0 and < 1000\n"
+    if str(size) == "" or size < 1 or size > 10000:
+        error += "Population Size has to be > 0 and < 10000\n"
+
+    return error
+
 
 @app.route("/end")
 def end():
--- simulator/simulation.py
+++ simulator/simulation.py
@@ -1,6 +1,56 @@
 class Simulation:
-    def __init__(self):
-        self.global
+    def __init__(self, populationSize, numberOfIterations, fitnessFunction):
+        self.globalID = 0
+        self.populationSize = populationSize
+        self.numberOfIterations = numberOfIterations
+        self.fitnessFunction = fitnessFunction
+        self.organisms = []
+        self.currentIterationNumber
+        self.currentIterationData = ""
+        self.simulationSpeed = 1.0
+        self.coordinates = {}
+
+    def displayInfo(self):
+        print(f"Population Size: {self.populationSize}\nTotal Iterations: {self.numberOfIterations}\nFitness Function: {self.fitnessFunction}")
+
+    def displaySimulation(self):
+        pass
+
+    def geneticAlgorithm(self):
+        pass
+
+    def addOrganism(self):
+        pass
+
+    def deleteOrganism(self):
+        pass
+
+    def updateIteration(self):
+        pass
+
+    def updateFitnessFunction(self, newFunction):
+        self.fitnessFunction = newFunction
+
+    def updateSimulationSpeed(self, newSpeed):
+        self.simulationSpeed = newSpeed
+
+    def createOrganism(self):
+        pass
+
+    def createGenes(self):
+        pass
+
+    def validateGenes(self):
+        pass
+
+    def validateCoordinates(self, xCoordinate, yCoordinate):
+        return (xCoordinate, yCoordinate) in self.coordinates
+
+    def decodeGenes(self):
+        pass
+
+    def updateDisplay(self):
+        pass
 
 
 class Organism:
